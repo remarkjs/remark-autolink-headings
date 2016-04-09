172459175bf03dd548d687709d31214130f46c30
Ben Briggs <beneb.info@gmail.com>
2016-04-09 15:44:38 +0100

    Update AVA to 0.14.0. Closes #12.

--- src/__tests__/index.js
+++ src/__tests__/index.js
@@ -11,11 +11,11 @@ let base = file => read(join(__dirname, 'fixtures', file), 'utf-8');
 test('should autolink headings', t => {
     ['append', 'prepend', 'wrap'].forEach(b => {
         let result = remark.use([slug, html]).use(headings, {behaviour: b}).process(base('input.md'));
-        t.same(result, base(`output.${b}.html`), `should ${b} headings`);
+        t.deepEqual(result, base(`output.${b}.html`), `should ${b} headings`);
     });
 });
 
 test('should do nothing if slugs are not used', t => {
     let result = remark.use([ headings, html ]).process(base('input.md'));
-    t.same(result, base('output.html'));
+    t.deepEqual(result, base('output.html'));
 });
